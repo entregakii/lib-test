a4e765619f52f3d897c8ef8ebcc7f32cc4a2542e
Benites <frecky60@gmail.com>
2023-05-19 10:55:29 -0300

    first commit

---
+++ src/LibText.tsx
@@ -0,0 +1,12 @@
+
+// import { Container } from './styles';
+
+interface Props {
+    text: string
+}
+
+const LibTest = ({ text }: Props) => {
+    return <div>{text}</div>;
+}
+
+export default LibTest;
